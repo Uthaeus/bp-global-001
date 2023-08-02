29e4b7e6a2ef623b9b5fcf896a7c8a131bb92a29
Uthaeus <romanlavery@gmail.com>
2023-08-02 11:16:55 -0600

    set up initial styling for layout, navigation, and footer

--- src/components/main-navigation.js
+++ src/components/main-navigation.js
@@ -1,11 +1,15 @@
 import { NavLink, Link } from "react-router-dom";
 
+import logo from '../assets/images/bp_global_log.png';
+
 function MainNavigation() {
 
     return (
         <div className="main-navigation">
             <div className="main-navigation-left">
-                <Link to='/' end>logo here</Link>
+                <Link to='/' end>
+                    <img src={logo} alt='nav-logo' />
+                </Link>
             </div>
 
             <div className="main-navigation-right">
@@ -14,9 +18,9 @@ function MainNavigation() {
                 </div>
 
                 <div className="navigation-links-wrapper">
-                    <NavLink to='/' end className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'}>Products</NavLink>
-                    <NavLink to='/' end className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'}>About Us</NavLink>
-                    <NavLink to='/' end className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'}>Contact Us</NavLink>
+                    <NavLink to='/' className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'} end>Products</NavLink>
+                    <NavLink to='/about' className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'}>About Us</NavLink>
+                    <NavLink to='/contact' className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'}>Contact Us</NavLink>
                 </div>
             </div>
         </div>
