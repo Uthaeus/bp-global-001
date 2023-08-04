3c004833738ecea89e9fb30db33dae6e62f68d39
Uthaeus <romanlavery@gmail.com>
2023-08-04 13:23:31 -0600

    added dropdown to navigation

--- src/components/main-navigation.js
+++ src/components/main-navigation.js
@@ -1,8 +1,22 @@
 import { NavLink, Link } from "react-router-dom";
+import { useState } from "react";
 
 import logo from '../assets/images/bp_global_log.png';
 
+// navbar-toggler
+
 function MainNavigation() {
+    const [showProducts, setShowProducts] = useState(false);
+
+    function openProducts() {
+        let element = document.getElementsByClassName('dropdown-toggle');
+        let drop = document.getElementsByClassName('dropdown-menu');
+
+        element.classList.add('show');
+        
+    }
+
+    function closeProducts() {}
 
     return (
         <div className="main-navigation">
@@ -16,7 +30,15 @@ function MainNavigation() {
                 <p className="navigation-phone">800-555-5555</p>
 
                 <div className="navigation-links-wrapper">
-                    <NavLink to='/' className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'} end>Products</NavLink>
+                    <p onMouseEnter={openProducts} onMouseLeave={closeProducts} className="dropdown-toggle nav-link" data-bs-toggle="dropdown" aria-expanded="false">
+                        Products
+                    </p>
+                    <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="/">Action</a></li>
+                        <li><a className="dropdown-item" href="/">Another action</a></li>
+                        <li><a className="dropdown-item" href="/">Something else here</a></li>
+                    </ul>
+
                     <NavLink to='/about' className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'}>About Us</NavLink>
                     <NavLink to='/contact' className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'}>Contact Us</NavLink>
                 </div>
