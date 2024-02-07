36ea03f7166bc4c7f127cbb574e7813e9f601d2f
Roman Lavery <romanlavery@Romans-MBP.lan>
2024-02-07 13:04:44 -0700

    added links to account sidebar, began initial styling

--- src/components/account/account-sidebar.js
+++ src/components/account/account-sidebar.js
@@ -3,10 +3,16 @@ import { Link } from "react-router-dom";
 export default function AccountSidebar() {
 
     return (
-        <>
-            <div className="account-sidebar">
-                <Link to='/account/profile'>Profile</Link>
-            </div>
-        </>
+        <div className="account-sidebar">
+            {/* account image */}
+
+            <Link to='/account/profile' className="sidebar-link">Profile</Link>
+
+            <Link to='/account/current-orders' className="sidebar-link">Current Orders</Link>
+
+            <Link to='/account/history' className="sidebar-link">Order History</Link>
+
+            <button className="sidebar-logout-btn">Logout</button>
+        </div>
     );
 }
