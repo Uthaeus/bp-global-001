826244e19e96131044fe9f98eb7d5a106b1f813e
Roman Lavery <romanlavery@Romans-MBP.lan>
2024-02-08 13:10:02 -0700

    added new lumber images

--- src/components/account/account-home.js
+++ src/components/account/account-home.js
@@ -5,6 +5,8 @@ export default function AccountHome() {
     return (
         <div className="account-home">
             <h1>Account Home</h1>
+
+            <Link to='/account/profile'>Profile</Link>
         </div>
     );
 }
