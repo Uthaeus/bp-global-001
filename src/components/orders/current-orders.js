9b6ccf400f9aeb7d33edc9887c2e2d37f5eb75f4
Uthaeus <romanlavery@gmail.com>
2024-02-20 11:03:29 -0700

    imported sample data to orders, began initial layout and styling for orders

--- src/components/orders/current-orders.js
+++ src/components/orders/current-orders.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
 
 import OrderChartHeader from "./order-chart-header";
 import OrderItem from "./order-item";
 
+import { sampleOrders } from "../../store/DUMMY_DATA";
+
 function CurrentOrders() {
+    const [orders, setOrders] = useState(sampleOrders.filter(order => order.orderStatus === "Shipped"));
+
     return (
         <div className="orders-container">
             <h1 className="orders-title">Current Orders</h1>
 
-            <OrderChartHeader />
+            <div className="orders-chart-container">
+                <OrderChartHeader />
+                {orders.map(order => (
+                    <OrderItem key={order.orderNumber} order={order} />
+                ))}
+            </div>
         </div>
     );
 }
--- src/components/orders/order-history.js
+++ src/components/orders/order-history.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
 
 import OrderChartHeader from "./order-chart-header";
 import OrderItem from "./order-item";
 
+import { sampleOrders } from "../../store/DUMMY_DATA";
+
 function OrderHistory() {
+    const [activeOrders, setActiveOrders] = useState(sampleOrders.filter(order => order.orderStatus === "Shipped"));
+    const [deliveredOrders, setDeliveredOrders] = useState(sampleOrders.filter(order => order.orderStatus === "Delivered"));
+
     return (
         <div className="orders-container">
             <h1 className="orders-title">Order History</h1>
 
             <div className="orders-chart-container">
                 <OrderChartHeader />
+                <p className="orders-chart-title">Active Orders</p>
+                {activeOrders.map(order => (
+                    <OrderItem key={order.orderNumber} order={order} />
+                ))}
+
+                <p className="orders-chart-title">Delivered Orders</p>
+                {deliveredOrders.map(order => (
+                    <OrderItem key={order.orderNumber} order={order} />
+                ))}
                 
             </div>
         </div>
