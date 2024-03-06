cfd5c374c62a6f274732bf85680bad15049686e5
Uthaeus <romanlavery@gmail.com>
2024-03-06 15:42:20 -0700

    created user context, imported to index

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.scss';
 import App from './App';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import UserContextProvider from './store/user-context';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </React.StrictMode>
 );
