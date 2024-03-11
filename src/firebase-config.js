e69ab938b94ceaaf71ec8f7eb26b077541e46df6
Uthaeus <romanlavery@gmail.com>
2024-03-11 11:34:16 -0600

    temporarily suspending firebase use until refactors are made in forms and context

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,19 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getStorage } from "firebase/storage";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID
-};
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_PROJECTID,
+//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//   appId: process.env.REACT_APP_APPID
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const storage = getStorage(app);
+// export const db = getFirestore(app);
+// export const auth = getAuth(app);
